a34e3221e3e8f40d54252d002e2e584d8f7ab581
Ahmet Faruk Yaşar <ahmetfaruk6420@gmail.com>
2025-01-03 12:07:53 +0300

    ip-2

--- laravel/app/Models/Reservation.php
+++ laravel/app/Models/Reservation.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     protected $fillable = [
+        'contact_id',
         'name',
         'email',
         'phone',
--- laravel/app/Models/Suggestion.php
+++ laravel/app/Models/Suggestion.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Suggestion extends Model
 {
     protected $fillable = [
+        'contact_id',
         'name',
         'email',
         'phone',
